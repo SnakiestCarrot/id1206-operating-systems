fde93377a70f52568e230ce57fee6810f7795552
Casper Johansson <147274068+SnakiestCarrot@users.noreply.github.com>
2025-12-02 11:06:42 +0100

    Screenshot taken with this version

--- Assignment3/question6/question6.c
+++ Assignment3/question6/question6.c
@@ -1,25 +1,32 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
+    // check for correct number of arguments
+    if(argc != 2) {
+        printf("Usage: %s <N>\n", argv[0]);
+        return 1;
+    }
+
     int N = atoi(argv[1]);
     
     // get page size in bytes, like the task hints at
     size_t page_size = getpagesize();
 
-    printf("The page size is %ld, N is %d and total allocated memory will be %ld bytes\n", page_size, N, N*page_size);
+    printf("The page size is %zu bytes, N is %d and total allocated memory will be %zu bytes\n", page_size, N, N*page_size);
 
     // make void pointer array for the allocated pages
-    void *allocated_pages[N];
+    void *ptr = malloc(N * page_size);
 
-    // allocate N pages of size page_size with malloc
-    for(int i = 0; i < N; i++) {
-        allocated_pages[i] = malloc(page_size);
+    if (ptr == NULL) {
+        perror("Malloc failed");
+        return 1;
     }
 
-    // Print the first memory position as a number to suppress unused warning
-    printf("Random memory location: %d\n", allocated_pages);
+    printf("Random memory location: %p\n", ptr);
 
+    free(ptr);
     return 0;
 }
